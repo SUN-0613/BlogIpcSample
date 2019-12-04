43529f4018ef083b8e5f5dc9edab829f43202789
AkihiroYamamoto <akiyam0613@gmail.com>
2019-12-04 11:28:13 +0900

    プロジェクト作成

---
+++ BlogIpcSample/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BlogIpcSample
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            base.OnStartup(e);
+
+            // Commonプロジェクト内のClass1クラスを参照
+            var common = new Common.Class1();
+
+        }
+
+    }
+}
