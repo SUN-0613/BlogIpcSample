6fc65ac91cf3686d287a87aac51a703d82fece7b
AkihiroYamamoto <akiyam0613@gmail.com>
2019-12-07 16:47:05 +0900

    Serverプロジェクト：作成

---
+++ Server/Forms/Model/ServerModel.cs
@@ -0,0 +1,60 @@
+﻿using Common.IPC;
+using System;
+using System.ServiceModel;
+using System.Threading;
+
+namespace Server.Forms.Model
+{
+
+    /// <summary>サーバ.Model</summary>
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServerModel : IProcess, IDisposable
+    {
+
+        /// <summary>IPC通信サービス</summary>
+        private ServiceHost _Host;
+
+        /// <summary>サーバModel</summary>
+        public ServerModel()
+        {
+
+            // IPC通信サービス開始
+            _Host = new ServiceHost(this, new Uri(Service.GetBaseAddress()));
+            _Host.AddServiceEndpoint(typeof(IProcess), new NetNamedPipeBinding(), Service.GetEndpoint());
+            _Host.Open();
+
+        }
+
+        /// <summary>解放処理</summary>
+        public void Dispose()
+        {
+
+            // IPC通信サービス終了
+            _Host.Close();
+
+        }
+
+        /// <summary>IPC通信処理実行</summary>
+        /// <param name="sec">処理秒数</param>
+        int IProcess.Execute(int sec)
+        {
+
+            var value = -1;
+            var random = new Random();
+
+            // 何か重たい処理
+            for (var i = 0; i < sec; i++)
+            {
+
+                Thread.Sleep(1000);
+                value = random.Next();
+
+            }
+
+            return value;
+
+        }
+
+    }
+
+}
---
+++ Server/Forms/ViewModel/ServerViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Common.MVVM;
+
+namespace Server.Forms.ViewModel
+{
+
+    /// <summary>サーバ.ViewModel</summary>
+    public class ServerViewModel : ViewModelBase
+    {
+
+        #region Model
+
+        /// <summary>サーバ.Model</summary>
+        private Model.ServerModel _Model;
+
+        #endregion
+
+        /// <summary>サーバ.ViewModel</summary>
+        public ServerViewModel()
+        {
+            _Model = new Model.ServerModel();
+        }
+
+    }
+
+}
